95886c7e7235f8d6704e3111239032a9727e7a50
Deepti <jannatul.ferdous.deepti@gmail.com>
2024-10-06 10:09:55 +0600

    first commit

---
+++ actions/stream.actions.ts
@@ -0,0 +1,25 @@
+ 'use server';
+
+import { currentUser } from "@clerk/nextjs/server";
+import { StreamClient } from "@stream-io/node-sdk";
+
+  
+ const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+ const apiSecret =  process.env.STREAM_SECRET_KEY;
+
+ export const tokenProvider = async () => {
+    const user = await currentUser();
+    
+    if (!user) throw new Error('User is not authenticated');
+    if (!apiKey) throw new Error('Stream API key secret is missing');
+    if (!apiSecret) throw new Error('Stream API secret is missing');
+
+    const client = new StreamClient(apiKey, apiSecret);
+
+    const validity = 60 * 60;
+    
+    const token = client.generateUserToken({ user_id: user.id, validity_in_seconds: validity });
+
+    return token;
+    
+}
